c8d3e4fc533788d853de3aff9cea2f63db3497a0
miss南敏 <396025608@qq.com>
2019-02-03 06:24:27 +0800

    提交

---
+++ 06 Sample/Redis.Core.Sample/Redis.Core.Sample.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace Redis.Core.Sample.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisHelper redisHelper = new RedisHelper(1);
+            redisHelper.KeyDelete("mykey");
+            Console.WriteLine("键盘输入:push入队,pop出队");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "push")
+                {
+                    Console.WriteLine("开始入队");
+                    for(int i = 0; i < 100; i++)
+                    {
+                        redisHelper.ListRightPush("pushQuene", i.ToString());
+                    }
+                    Console.WriteLine("完成入队");
+                }
+                else if (input == "pop")
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        if (redisHelper.ListLength("pushQuene") > 0)
+                        {
+                            Console.WriteLine(redisHelper.ListLeftPop("pushQuene"));
+                        }
+                        else { Console.WriteLine("队列为空"); }
+                    }
+                }
+            }
+        }
+    }
+}
